40427b6116dff8f96b37c2b814ac050b54ea0702
South <kimhyoungnams@gmail.com>
2020-02-10 20:54:16 +0900

    add dotenv at server.js

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
-const express = require('express');
-const path = require('path');
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
 const app = express();
-​
-app.get('/ping', function(req, res) {
-  res.send('pong');
+app.get("/ping", function(req, res) {
+  res.send("pong");
 });
-​
-app.use('/', express.static(path.join(__dirname, 'build')));
-​
-app.get('/*', function (req, res) {
+app.use("/", express.static(path.join(__dirname, "build")));
+app.get("/*", function(req, res) {
   res.set({
     "Cache-Control": "no-cache, no-store, must-revalidate",
-    "Pragma": "no-cache",
-    "Date": new Date()
+    Pragma: "no-cache",
+    Date: new Date()
   });
-​
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-​
-app.listen(80, () => {
-  console.log('success!');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("success!");
 });
